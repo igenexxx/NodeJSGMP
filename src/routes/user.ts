633b7f77bc60dffb2043dbba95df6fc860dc5f4f
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-05-24 23:27:31 +0400

    feature/task-2.2: added validator

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import { create, remove, suggest, update } from '../controllers/user';
+import { querySchema, validator } from '../validators/user';
 
 const router = Router();
 
-router.post('/', create);
-router.put('/:id', update);
+router.post('/', validator.body(querySchema), create);
+router.put('/:id', validator.body(querySchema), update);
 router.get('/suggest', suggest);
 router.delete('/:id', remove);
 
