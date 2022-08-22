210f83d0bdb39d3f03b7f0086ca21ef315b2b132
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-08-22 14:56:45 +0300

    feature/task-7.1_7.2: fixed config

--- src/controllers/__tests__/user.spec.ts
+++ src/controllers/__tests__/user.spec.ts
@@ -1,14 +1,30 @@
+import request from 'supertest';
+
+import { app } from '../../app';
 import { myContainer } from '../../config/inversify.config';
+import { mockUserService } from '../../mocks/services/mockUserService';
+import { userRoutePath } from '../../routes/user';
+import { UserService } from '../../services/user.service';
 import { UserController } from '../user';
 
 describe('User', () => {
-  let user: UserController;
+  let userController: UserController;
 
   beforeEach(() => {
-    user = myContainer.get(UserController);
+    userController = myContainer.get(UserController);
+    myContainer.rebind(UserService).toConstantValue({
+      ...mockUserService,
+      getAllUsers: jest.fn().mockResolvedValue([{ id: 1, login: 'test', age: 20, password: 'test' }]),
+    });
   });
 
   it('should be defined', () => {
-    expect(user).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  it('should return list of users', async () => {
+    const response = await request(app).get(userRoutePath).send();
+
+    expect(response.status).toBe(200);
   });
 });
---
+++ src/mocks/services/mockUserService.ts
@@ -0,0 +1,12 @@
+import type { UserService } from '../../services/user.service';
+
+type MockUserServiceModel = { [key in keyof UserService]: jest.Mock };
+
+export const mockUserService: MockUserServiceModel = {
+  getAllUsers: jest.fn(),
+  createUser: jest.fn(),
+  updateUser: jest.fn(),
+  removeUser: jest.fn(),
+  suggest: jest.fn(),
+  validateUser: jest.fn(),
+};
